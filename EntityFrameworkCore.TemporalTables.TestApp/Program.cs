a8e7282ab851e8fe839c592900464c40737282a3
Isaac Wilson (B406) <Isaac.Wilson@bcbsks.com>
2021-02-01 10:47:59 -0600

    Added simple user tests to program

--- EntityFrameworkCore.TemporalTables.TestApp/Program.cs
+++ EntityFrameworkCore.TemporalTables.TestApp/Program.cs
@@ -1,9 +1,12 @@
 ﻿using EntityFrameworkCore.TemporalTables.Extensions;
 using EntityFrameworkCore.TemporalTables.Sql;
+using EntityFrameworkCore.TemporalTables.TestApp.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace EntityFrameworkCore.TemporalTables.TestApp
 {
@@ -14,9 +17,11 @@ namespace EntityFrameworkCore.TemporalTables.TestApp
             IConfigurationRoot configuration = new ConfigurationBuilder()
               .SetBasePath(Directory.GetCurrentDirectory())
               .AddJsonFile("appsettings.json")
+              .AddUserSecrets(typeof(Program).Assembly)
               .Build();
 
             IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<Tests>();
 
             services.AddDbContextPool<DataContext>((provider, options) =>
             {
@@ -43,6 +48,55 @@ namespace EntityFrameworkCore.TemporalTables.TestApp
             // Execute the temporal tables SQL code against the database (SQL code is generated internally without exposing to outside).
             var temporalTableSqlExecutor = serviceProvider.GetService<ITemporalTableSqlExecutor<DataContext>>();
             temporalTableSqlExecutor.Execute();
+
+            serviceProvider.GetService<Tests>().RunTests();
+        }
+    }
+
+    //TODO: move tests into test projects
+    public class Tests
+    {
+        private IServiceProvider Services { get; set; }
+        private DataContext DbContext { get; set; }
+
+        public Tests(IServiceProvider services, DataContext dbContext)
+        {
+            Services = services;
+            DbContext = dbContext;
+        }
+
+        public void RunTests()
+        {
+            Test1();
+        }
+
+        public void Test1()
+        {
+            DbContext.Users.RemoveRange(DbContext.Users);
+            DbContext.SaveChanges();
+            string firstPassword = "StrongPassword1";
+            var user = new User();
+            user.UserName = "NewUser1";
+            user.Password = firstPassword;
+
+            DbContext.Add(user);
+            DbContext.SaveChanges();
+
+            DateTime firstTime = DateTime.Now.ToUniversalTime();
+
+            System.Threading.Thread.Sleep(1000);
+            user.Password = "StrongerPassword123";
+
+            DbContext.SaveChanges();
+
+            var oldUser = DbContext.Users.AsOf(firstTime).FirstOrDefault(u => u.Id == user.Id);
+            if(oldUser is null || !string.Equals(oldUser.Password, firstPassword))
+            {
+                throw new Exception("Something's broken!");
+            }
+
+            var userHistory = DbContext.Users.ForAll();
+            Console.WriteLine(userHistory.Count());
         }
     }
 }
